34b7c121a04b83949272fc0353ad3a1d6a3fba78
shaniaesguerra <shaniae47@gmail.com>
2026-07-25 10:53:45 -0600

    W04 Code Linked Lists Problem 5 completed

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -214,7 +214,12 @@ public class LinkedList : IEnumerable<int>
     public IEnumerable Reverse()
     {
         // TODO Problem 5
-        yield return 0; // replace this line with the correct yield return statement(s)
+        var curr = _tail; // Start at the end
+        while (curr is not null)
+        {
+            yield return curr.Data; // Provide (yield) each item to the user
+            curr = curr.Prev; // Go backwards in the linked list
+        }
     }
 
     public override string ToString()
